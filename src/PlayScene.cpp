bf7f316b2713734bbb8496e6b841eac7a5fc2446
gildobro <gdobrovinsky@gmail.com>
2020-07-18 21:29:13 -0400

    Added Debugger Mode

--- src/PlayScene.cpp
+++ src/PlayScene.cpp
@@ -119,11 +119,67 @@ void PlayScene::handleEvents()
 			}
 		}
 	}
+
+	if (!m_bDebugKeys[H_KEY])
+	{
+		if (EventManager::Instance().isKeyDown(SDL_SCANCODE_H))
+		{
+			m_bDebugMode = !m_bDebugMode;
+
+			m_bDebugMode[H_KEY] = true;
+
+			if (m_bDebugMode)
+			{
+				std::cout << "Debug Mode On" << std::endl;
+			}
+			else if (!m_bDebugMode)
+			{
+				std::cout << "Debug Mode off" << std::endl;
+			}
+		}
+	}
+
+	if (EventManager::Instance().isKeyUp(SDL_SCANCODE_H))
+	{
+		m_bDebugKeys[H_KEY] = false;
+	}
+
+	if (!m_bDebugKeys[K_KEY])
+	{
+		if (EventManager::Instance().isKeyDown(SDL_SCANCODE_K))
+		{
+			std::cout << "DEBUG: Enemy Takes Damage!!" << std::endl;
+		}
+	}
+
+	if (EventManager::Instance().isKeyUp(SDL_SCANCODE_K))
+	{
+		m_bDebugKeys[K_KEY] = false;
+	}
+
+	if (!m_bDebugKeys[P_KEY])
+	{
+		if (EventManager::Instance().isKeyDown(SDL_SCANCODE_P))
+		{
+			m_bPatrolMode = !m_bPatrolMode;
+
+			m_bDebugKeys[P_KEY] = true;
+
+			if (m_bPatrolMode)
+			{
+				std::cout << "DEBUG: Patrol Mode On" << std::endl;
+			}
+			else if (!m_bPatrolMode)
+			{
+				std::cout << "DEBUG: Patrol Mode off" << std::endl;
+			}
+		}
+	}
 	
 
-	if (EventManager::Instance().isKeyDown(SDL_SCANCODE_ESCAPE))
+	if (EventManager::Instance().isKeyDown(SDL_SCANCODE_P))
 	{
-		TheGame::Instance()->quit();
+		m_bDebugKeys[P_KEY] = false;
 	}
 
 	if (EventManager::Instance().isKeyDown(SDL_SCANCODE_1))
